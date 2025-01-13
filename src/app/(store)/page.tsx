73c5211fd20b32d6f8591b09aee4b4fdc4c967ed
rayhankobir-dev <rayhankobir893@gmail.com>
2025-01-14 05:49:06 +0600

    feat: top selling medicines

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/home/hero";
 import HowToOderder from "@/components/home/how-to-order";
 import CtaMenuSection from "@/components/home/cta-menus";
+import TopSelling from "@/components/home/top-selling";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <CtaMenuSection />
       <HowToOderder />
+      <TopSelling />
     </main>
   );
 }
--- src/components/home/top-selling.tsx
+++ src/components/home/top-selling.tsx
@@ -1,13 +1,22 @@
+import api from "@/api";
 import React from "react";
-import ProductCategorySection from "./product-category";
+import { Medicine } from "@/types";
+import MedicineCard from "../medicine/medicine-card";
 
-function TopSelling() {
+async function TopSelling() {
+  const res = await api.get("/medicines?limit=4");
+  const { medicines } = res.data;
+
   return (
-    <section>
-      <h1>Top Selling Medicines</h1>
+    <section className="py-5">
+      <h1 className="font-semibold text-2xl mb-6 opacity-80">
+        Top Selling Medicines
+      </h1>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-        <ProductCategorySection />
+        {medicines.map((medicine: Medicine) => (
+          <MedicineCard key={medicine._id} medicine={medicine} />
+        ))}
       </div>
     </section>
   );
